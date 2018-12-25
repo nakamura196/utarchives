d4bf005be3311d3f05de954f3fb62152e4a2dfa0
satoru196 <na.kamura.1263@gmail.com>
2018-12-25 15:18:25 +0900

    add export script

---
+++ src/export/ExcelGenerator.py
@@ -0,0 +1,163 @@
+import sys
+import urllib
+import json
+import argparse
+import urllib.request
+import unicodedata
+import pandas as pd
+
+
+def parse_args(args=sys.argv[1:]):
+    """ Get the parsed arguments specified on this script.
+    """
+    parser = argparse.ArgumentParser(description="")
+
+    parser.add_argument(
+        'output_path',
+        action='store',
+        type=str,
+        help='Path to output file.')
+
+    return parser.parse_args(args)
+
+
+def excel_generator():
+    label_map = dict()
+    # label_map["bibo:identifier"] = "ID"
+    label_map["dcterms:isPartOf"] = "ウェブサイトURL"
+    label_map["dcterms:relation"] = "アイテムURL"
+    label_map["dcterms:rights"] = "利用条件"
+    label_map["dcterms:title"] = "タイトル"
+    label_map["foaf:thumbnail"] = "サムネイル"
+    label_map["rdfs:seeAlso"] = "機械可読ドキュメント"
+    label_map["sc:attributionLabel"] = "帰属"
+    label_map["sc:viewingDirection"] = "viewingDirection"
+    # label_map["uterms:databaseLabel"] = "コレクション"
+    label_map["uterms:manifestUri"] = "IIIFマニフェストURI"
+    # label_map["uterms:sort"] = "ソート用項目"
+    # label_map["uterms:year"] = "年"
+
+    output_path = "data/data.xlsx"
+
+    table = []
+    rows = []
+    keySet = {}
+    template_arr = []
+    sorted_keys = []
+
+    api_url = "https://webpark5029.sakura.ne.jp/uta/api"
+
+    loop_flg = True
+    page = 1
+
+    while loop_flg:
+        url = api_url + "/items?page=" + str(
+            page) + "&sort_by=uterms%3Asort&sort_order=asc"
+        print(url)
+
+        page += 1
+
+        request = urllib.request.Request(url)
+        response = urllib.request.urlopen(request)
+
+        response_body = response.read().decode("utf-8")
+        data = json.loads(response_body.split('\n')[0])
+
+        if len(data) > 0:
+            for i in range(len(data)):
+
+                obj = data[i]
+
+                if obj["o:resource_template"] != None:
+
+                    template_id = obj["o:resource_template"]["@id"]
+                    if template_id not in template_arr:
+                        template_arr.append(template_id)
+
+                for key in obj:
+                    if not key.startswith("o:") and key != "@type":
+                        if key not in keySet and isinstance(obj[key], list):
+                            if "property_label" in obj[key][0]:
+                                keySet[key] = obj[key][0]["property_label"]
+
+                rows.append(obj)
+
+        else:
+            loop_flg = False
+
+    for template_id in template_arr:
+        response = urllib.request.urlopen(template_id)
+        response_body = response.read().decode("utf-8")
+        data = json.loads(response_body.split('\n')[0])
+        property_arr = data["o:resource_template_property"]
+        for property in property_arr:
+
+            property_label = property["o:alternate_label"]
+
+            property_id = property["o:property"]["@id"]
+
+            response = urllib.request.urlopen(property_id)
+            response_body = response.read().decode("utf-8")
+            data = json.loads(response_body.split('\n')[0])
+
+            term = data["o:term"]
+
+            sorted_keys.append(term)
+
+            if property_label:
+                label_map[term] = property_label
+
+    for term in label_map:
+        if term not in sorted_keys:
+            sorted_keys.append(term)
+
+    for term in sorted(keySet):
+        if term not in sorted_keys:
+            sorted_keys.append(term)
+
+    row = []
+    table.append(row)
+    row.append("Omeka ID")
+    row.append("Omeka Created")
+    row.append("Omeka Modified")
+    for term in sorted_keys:
+        if term in label_map:
+            row.append(label_map[term])
+        else:
+            row.append(term)
+
+    # 二行目以降
+    for obj in rows:
+        row = []
+        table.append(row)
+
+        row.append(obj["o:id"])
+        row.append(obj["o:created"]["@value"].split("T")[0])
+        row.append(obj["o:modified"]["@value"].split("T")[0])
+
+        for term in sorted_keys:  # sorted(keySet):
+            text = ""
+            if term in obj:
+                values = obj[term]
+                for i in range(len(values)):
+                    value = values[i]
+                    if "@value" in value:
+                        text += value["@value"]
+                    else:
+                        text += "=\"" + value["@id"] + "\""
+                    if i != len(values) - 1:
+                        text += "|"
+            row.append(unicodedata.normalize("NFKC", text))
+
+    df = pd.DataFrame(table);
+
+    df.to_excel(output_path, index=False, header=False)
+    df.to_csv(output_path.replace("xlsx", "csv"), index=False, header=False)
+    df.to_csv(output_path.replace("xlsx", "tsv"), index=False, header=False, sep='\t')
+
+
+if __name__ == "__main__":
+    # args = parse_args()
+
+    # excel_generator(args.output_path)
+    excel_generator()
